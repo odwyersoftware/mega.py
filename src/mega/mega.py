5fffdd00da651f9eb4e52dbbd4e8da196b50ca5a
MaxTheBreaker <MaxTheBreaker@users.noreply.github.com>
2020-09-21 07:03:12 +0300

    Fix permission denied in download function
    Fixes issue #18 caused by trying to move the temporary file that is still open

--- src/mega/mega.py
+++ src/mega/mega.py
@@ -741,9 +741,9 @@ class Mega:
             if (file_mac[0] ^ file_mac[1],
                     file_mac[2] ^ file_mac[3]) != meta_mac:
                 raise ValueError('Mismatched mac')
-            output_path = Path(dest_path + file_name)
-            shutil.move(temp_output_file.name, output_path)
-            return output_path
+        output_path = Path(dest_path + file_name)
+        shutil.move(temp_output_file.name, output_path)
+        return output_path
 
     def upload(self, filename, dest=None, dest_filename=None):
         # determine storage node
