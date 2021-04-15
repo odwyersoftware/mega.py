4dc5f8dee1a0da95fa1500003a4591106c43ac65
Tuqay Abdullazade <57816441+tuqayabdulla@users.noreply.github.com>
2021-04-15 15:01:27 +0400

    fixed async once again

--- src/mega/mega.py
+++ src/mega/mega.py
@@ -152,7 +152,7 @@ class Mega:
 
     @retry(retry=retry_if_exception_type(RuntimeError),
            wait=wait_exponential(multiplier=2, min=2, max=60))
-    def _api_request(self, data):
+    async def _api_request(self, data):
         params = {'id': self.sequence_num}
         self.sequence_num += 1
 
@@ -164,12 +164,13 @@ class Mega:
             data = [data]
 
         url = f'{self.schema}://g.api.{self.domain}/cs'
-        response = requests.post(
-            url,
-            params=params,
-            data=json.dumps(data),
-            timeout=self.timeout,
-        )
+        async with self.client as client:
+            response = client.post(
+                url,
+                params=params,
+                data=json.dumps(data),
+                timeout=self.timeout,
+            )
         json_resp = json.loads(response.text)
         try:
             if isinstance(json_resp, list):
@@ -758,7 +759,7 @@ class Mega:
         # request upload url, call 'u' method
         with open(filename, 'rb') as input_file:
             file_size = os.path.getsize(filename)
-            ul_url = self._api_request({'a': 'u', 's': file_size})['p']
+            ul_url = await self._api_request({'a': 'u', 's': file_size})['p']
             # generate random aes key (128) for file
             ul_key = [random.randint(0, 0xFFFFFFFF) for _ in range(6)]
             k_str = a32_to_str(ul_key[:4])
@@ -796,8 +797,8 @@ class Mega:
 
                     # encrypt file and upload
                     chunk = aes.encrypt(chunk)
-                    async with self.client:
-                        output_file = await self.client.post(ul_url + "/" +
+                    async with self.client as client:
+                        output_file = await client.post(ul_url + "/" +
                                                 str(chunk_start),
                                                 data=chunk,
                                                 timeout=self.timeout)
@@ -805,8 +806,8 @@ class Mega:
                     logger.info('%s of %s uploaded', upload_progress,
                                 file_size)
             else:
-                async with self.client:
-                    output_file = self.client.post(ul_url + "/0",
+                async with self.client as client:
+                    output_file = client.post(ul_url + "/0",
                                             data='',
                                             timeout=self.timeout)
                 completion_file_handle = output_file.text
@@ -832,7 +833,7 @@ class Mega:
             encrypted_key = a32_to_base64(encrypt_key(key, self.master_key))
             logger.info('Sending request to update attributes')
             # update attributes
-            data = self._api_request({
+            data = await self._api_request({
                 'a':
                 'p',
                 't':
